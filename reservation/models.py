925d4fdccc5637a42e8405bd4668df08085f0487
DevIsis <kitcher96@gmail.com>
2022-03-24 16:33:37 +0000

    Add customer model and make initial migrations

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    customerId = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=25, null=False, blank=False)
+    surname = models.CharField(max_length=25, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    password = models.CharField(max_length=50, null=False, blank=False)
+    phone = models.CharField(max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        return self.email
