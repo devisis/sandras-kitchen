5b0215e303ab021407ae9490ffab96d1660c1574
DevIsis <kitcher96@gmail.com>
2022-07-19 16:26:09 +0000

    Add update functionality add delete fuctionality both for admin

--- reservation/urls.py
+++ reservation/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.reservation, name="reservation"),
-    path('my_reservations/', views.EditReservations, name="my_reservations"),
+    path('create', views.reservation_create, name="reservation_create"),
+    path('details', views.reservation_details, name="reservation_details"),
+    path('edit/<res_id>/', views.reservation_edit, name="reservation_edit"),
+    path('delete/<res_id>/', views.reservation_delete, name="reservation_delete"),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,26 +1,51 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import path
 from .forms import ReservationForm
 from.models import Reservation
-from django.views.generic import ListView
 
 
-def reservation(request):
+def reservation_create(request):
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
             form.save()
             return render(request, 'home/index.html')
-        else:
-            return render(request, 'reservation.html', {'form': form})
+        # else:
+        #     return render(request, 'reservation_create.html', {'form': form})
             # redirect('home/index.html')
 
     form = ReservationForm()
     context = {
         'form': form
     }
-    return render(request, 'reservation.html', context)
+    return render(request, 'reservation_create.html', context)
 
 
-class EditReservations(ListView):
-    model = Reservation
+def reservation_details(request):
+    form = Reservation.objects.all()
+    context = {
+        'form': form
+    }
+    return render(request, 'reservation_details.html', context)
+
+
+def reservation_edit(request, res_id):
+    reservation = get_object_or_404(Reservation, id=res_id)
+    if request.method == 'POST':
+        form = ReservationForm(request.POST, instance=reservation)
+        if form.is_valid():
+            form.save()
+            return redirect('reservation_details')
+
+    form = ReservationForm(instance=reservation)
+    context = {
+        'form': form
+    }
+    return render(request, 'reservation_edit.html', context)
+
+
+def reservation_delete(request, res_id):
+    reservation = get_object_or_404(Reservation, id=res_id)
+    reservation.delete()
+
+    return redirect('reservation_details')
