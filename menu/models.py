76a1b9ff0c13a9b8085bd1a4c9896f5bb2b42e95
DevIsis <kitcher96@gmail.com>
2022-07-20 13:13:03 +0000

    Add comments remove unnecessary code in menu urls add menu styling

--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 
 class Menu(models.Model):
 
+    # defining menu types
     MENU_TYPE = (
         ('Starter', 'starter'),
         ('Main', 'main'),
--- menu/urls.py
+++ menu/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import MenuView
 
-app_nam = 'menu'
 
 urlpatterns = [
     path('', MenuView.as_view(), name='menu'),
