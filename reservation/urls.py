c37b9ee58802f037ad46e5903d4053be6f7ff7b3
DevIsis <kitcher96@gmail.com>
2022-08-10 21:30:26 +0000

    Add validation for multiple bookings for same day

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', login_required(views.AddReservationView.as_view()), name='reservation'),
-    path('details/', login_required(views.ReservationListView.as_view()), name='reservation_details'),
-    path('edit/<pk>/', login_required(views.ReservationUpdateView.as_view()), name='reservation_edit'),
+    path('list/', login_required(views.ReservationListView.as_view()), name='reservation_list'),
+    path('update/<pk>/', login_required(views.ReservationUpdateView.as_view()), name='reservation_update'),
     path('delete/<pk>/', login_required(views.ReservationDeleteView.as_view()), name='reservation_delete'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.urls import path, reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -9,9 +10,10 @@ from .models import Reservation
 from django.forms import ModelForm
 from django import forms
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import ValidationError
 
+from django.contrib.auth.models import User
 
-# Create form data instances
 
 class AddReservationView(SuccessMessageMixin, CreateView):
 
@@ -21,14 +23,24 @@ class AddReservationView(SuccessMessageMixin, CreateView):
     success_url = '/reservation/list/'
     success_message = 'Reservation created!'
 
-    def get_context_data(self):
-        context = super().get_context_data(**kwargs)
-
     # Set reservation user to current logged in user and ensure form is valid before save
     def form_valid(self, form):
         form.instance.user = self.request.user
-        self.object = form.save()
-        return super().form_valid(form)
+        user = User.objects.get(username=self.request.user.username)
+        userReservations = Reservation.objects.filter(user=user)
+        error = False
+        # loop through the reservations
+        for reservation in userReservations:
+            # check if any are the same as the date in the form
+            if str(reservation.date) == self.request.POST["date"]:
+                error = True
+
+        if error is True:
+            form.add_error('date', 'You have booked this day already. Try another date.')
+            return super(AddReservationView, self).form_invalid(form)
+        else:
+            self.object = form.save()
+            return super().form_valid(form)
 
 
 # Display a list of all reservations
@@ -52,7 +64,7 @@ class ReservationUpdateView(SuccessMessageMixin, UpdateView):
 
     model = Reservation
     form_class = ReservationForm
-    template_name = 'reservation_update.html'
+    template_name = 'reservation_update_form.html'
     success_url = '/reservation/list/'
     success_message = 'Reservation updated!'
 
